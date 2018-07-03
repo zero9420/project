f1a5dc80fc2955c799e5c436f0aa5b0f9fffe2be
zero9420 <1115816263@qq.com>
2018-07-03 10:04:42 +0800

    解决路由冲突

--- routes/web.php
+++ routes/web.php
@@ -23,18 +23,13 @@ Route::get('/', function () {
 
 Route::group([],function(){
 	// 后台首页
-<<<<<<< HEAD
 	Route::any('admin','admin\IndexController@index');
 	// 商品分类
 	Route::resource('admin/cate','admin\CateController');
 	// 商品管理
 	Route::resource('admin/goods','admin\GoodsController');
-=======
-		Route::any('admin','admin\IndexController@index');
-		//角色管理
-		Route::resource('admin/auth','admin\AuthController');
-
->>>>>>> origin/bin
+	// 角色管理
+	Route::resource('admin/auth','admin\AuthController');
 });
 
 
