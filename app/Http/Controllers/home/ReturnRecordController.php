cf95104924bfdd0ef5a84ac2b0da1a5313ab442a
getstrongcode <strongcode@sina.com>
2018-07-19 10:32:05 +0800

    dsfds

--- app/Http/Controllers/home/ReturnRecordController.php
+++ app/Http/Controllers/home/ReturnRecordController.php
@@ -50,27 +50,23 @@ class ReturnRecordController extends Controller
     	$user_id = session('user_id');
 
     	$res = Info::where('info_cid',$user_id)->first();
-
-        if(empty($res)){
-
-
+        if (empty($res)) {
             return back();
         } else {
+            // 根据当前登录用户获取shop_order 表用户ID信息  查询所有订单
 
-    	// 根据当前登录用户获取shop_order 表用户ID信息  查询所有订单
+        $order = DB::table('shop_order')->where('order_info_cid',$user_id)->where('order_id', 'like','%'.$request->input('order_id').'%')->paginate(10);
 
-    	$order = DB::table('shop_order')->where('order_info_cid',$user_id)->where('order_id', 'like','%'.$request->input('order_id').'%')->paginate(10);
+        // 根据查询到的所有订单 再去筛选 order_return_goods 大于1的
+        $detail = [];
+        foreach ($order as $k => $v) {
 
-    	// 根据查询到的所有订单 再去筛选 order_return_goods 大于1的
-    	$detail = [];
-    	foreach ($order as $k => $v) {
-
-    		$detail[] = DB::table('shop_order_detail')->where('order_id',$v->order_id)->where('order_return_goods','>=', 1)->get();
-    	}
+            $detail[] = DB::table('shop_order_detail')->where('order_id',$v->order_id)->where('order_return_goods','>=', 1)->get();
+        }
 
 
         }
-
+    	
     	return view('home.record.record',['detail'=>$detail,'res'=>$res,'title'=>'退货退款记录']);
 
 
